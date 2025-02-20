38ddb42371350c4e413375c0ca4f428109dbe529
Daria95-1 <dariabasynina@gmail.com>
2025-02-21 00:59:41 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const tailwindConfig = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: { extend: { colors: { mainBlue: '#2B8AFF' } } },
+    plugins: [],
+}
+
+export default tailwindConfig
