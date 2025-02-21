c0c8ed164d93f16b29f2cc1420d23c0691262b35
Daria95-1 <dariabasynina@gmail.com>
2025-02-21 18:42:53 +0300

    create main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+// json-server --watch src/db.json --port 5180 - запуск БД
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-// json-server --watch src/db.json --port 5180
+
+const Header = () => <div>Хедер</div>
+const Footer = () => <div>Футер</div>
 
 function App() {
-
-
   return (
     <>
-      <div>123</div>
+      <Header />
+      <div>
+        <h2>Контент страницы</h2>
+        <Routes>
+          <Route path='/register' element={<div>Регистрация</div>} />
+          <Route path='/login' element={<div>Авторизация</div>} />
+          {/* <Route path='*' element={<div>Ошибка</div>} /> */}
+        </Routes>
+      </div>
+      <Footer />
       
     </>
   )
